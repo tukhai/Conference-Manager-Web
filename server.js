75d313364e9e9e95b1cbbd4b31fe5d8c6388bcaf
Admin <admin@Admins-MacBook-Pro.local>
2018-12-25 03:08:02 +0800

    test post request for nodejs

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    // res.send('Hello Express!');
-    // res.send({
-    //     name: 'KKK',
-    //     likes: [
-    //         'Biking',
-    //         'Cities'
-    //     ]
-    // });
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         currentYear: new Date().getFullYear()
+    });
+});
+
+app.post('/talks-track-process', (req, res) => {
+    console.log(req.body);
+    res.send({
+        name: "sending back data from post -- " + req.body.content,
+        likes: [
+            'Biking',
+            'Cities'
+        ]
     });
 });
 
